e82432bebec891d8139544cdf7afa968bb5609d3
Yuburov <yubur.aziz@gmail.com>
2019-11-16 13:47:44 +0600

    Создал главную страницу, страницу просмотра и страницу создания товара

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
+from webapp.views import IndexView, ProductView, ProductCreateView
 
 app_name = 'webapp'
 
 urlpatterns = [
-
+    path('', IndexView.as_view(), name='index'),
+    path('products/<int:pk>/', ProductView.as_view(), name='product_detail'),
+    path('products/create/', ProductCreateView.as_view(), name='product_create')
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
+
+from webapp.models import Product
+
+
+class IndexView(ListView):
+    model = Product
+    template_name = 'product/index.html'
+
+class ProductView(DetailView):
+    model = Product
+    template_name = 'product/detail.html'
+
+class ProductCreateView(CreateView):
+    model = Product
+    template_name = 'product/create.html'
+    fields = ('name', 'category', 'description', 'image')
+    success_url = reverse_lazy('webapp:index')
