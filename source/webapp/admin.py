75f106d6f228629ef866a91117da881c046dbff0
Yuburov <yubur.aziz@gmail.com>
2019-11-16 13:07:51 +0600

    Созданы модели Товар и Отзывы. Также создана фикстура

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from webapp.models import Product, Review
+
+admin.site.register(Product)
+admin.site.register(Review)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,47 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-#
+# Create your models here.
+
+CATEGORY_CHOICES = (
+    ('other', 'Другое'),
+    ('food', 'Еда'),
+    ('clothes', 'Одежда'),
+    ('electronics', 'Электроника'),
+)
+
+MARK_CHOICES = (
+    ('1', '1'),
+    ('2', '2'),
+    ('3', '3'),
+    ('4', '4'),
+    ('5', '5')
+)
+
+class Product(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Товар')
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, default=CATEGORY_CHOICES[0][0],
+                                verbose_name='Категория')
+    description = models.TextField(max_length=2000, null=True, blank=True, verbose_name='Описание')
+    image = models.ImageField(null=True, blank=True, upload_to='product_images', verbose_name='Фото')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+
+class Review(models.Model):
+    author = models.ForeignKey(User, related_name='reviews', on_delete=models.CASCADE, verbose_name='Пользователь')
+    product = models.ForeignKey('webapp.Product', related_name='reviews', on_delete=models.CASCADE, verbose_name='Товар')
+    text = models.TextField(max_length=3000, verbose_name='Текст отзыва')
+    mark = models.IntegerField(null=False, blank=False, choices=MARK_CHOICES, verbose_name='Оценка')
+
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
+
